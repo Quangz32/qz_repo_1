08b59644cf130374f547b56ebff184b5674aa47e
Hồng Quang Vũ <quangzdrive@gmail.com>
2024-01-17 17:36:14 +0700

    init

---
+++ src/Controller/MyController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MyController extends AbstractController
+{
+    #[Route('/my', name: 'app_my')]
+    public function index(): Response
+    {
+        return $this->render('my/index.html.twig', [
+            'controller_name' => 'MyController',
+        ]);
+    }
+    
+    /**
+     * oldMethod
+     *
+     * @Route("/old", name="old")
+     */
+    public function oldMethod(): Response {
+        return $this->render('my/index.html.twig', [
+            'controller_name' => 'MyController',
+        ]);
+    }
+}
